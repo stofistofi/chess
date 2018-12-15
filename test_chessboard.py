e6be585779b1581045a0476301810ad4e1bda486
Kristófer Gudmundsson <kristofergudmundsson@Kristofers-MacBook-Pro.local>
2018-12-15 18:39:37 +0000

    rook, bishop, queen, king tests

--- test_chessboard.py
+++ test_chessboard.py
@@ -180,13 +180,115 @@ def test_diagonal_travel():
     except:
         return False
 
+def test_rookValidity():
+    c = Chessboard()
+    c.move_piece(48,32)     # p to A4
+    c.move_piece(15,31)     # P to H5
+    c.move_piece(56,40)     # r to A3
+    c.move_piece(7,23)      # R to H6
+    c.move_piece(51,43)     # p to D3
+    c.move_piece(11,19)     # P to D6
+    '''{ 0:'R',  1:'N',  2:'B',  3:'Q',  4:'K',  5:'B',  6:'N',  7:' ', 
+         8:'P',  9:'P', 10:'P', 11:' ', 12:'P', 13:'P', 14:'P', 15:' ',
+        16:' ', 17:' ', 18:' ', 19:'P', 20:' ', 21:' ', 22:' ', 23:'R',
+        24:' ', 25:' ', 26:' ', 27:' ', 28:' ', 29:' ', 30:' ', 31:'P',
+        32:'p', 33:' ', 34:' ', 35:' ', 36:' ', 37:' ', 38:' ', 39:' ',
+        40:'r', 41:' ', 42:' ', 43:'p', 44:' ', 45:' ', 46:' ', 47:' ',
+        48:' ', 49:'p', 50:'p', 51:' ', 52:'p', 53:'p', 54:'p', 55:'p',
+        56:' ', 57:'n', 58:'b', 59:'q', 60:'k', 61:'b', 62:'n', 63:'r'}'''
+    try:
+        assert c.rookValidity(40,41) == True        # move to the side
+        assert c.rookValidity(40,44) == False       # try to jump over p
+        assert c.rookValidity(40,48) == True        # go back one square
+        assert c.rookValidity(40,33) == False       # try going diagonally
+        assert c.rookValidity(40,39) == False       # try going off board to the side
+        return True
+    except:
+        return False
 
-# print("Testing create_board():     ", test_create_board())
-# print("Testing current_board():    ",test_current_board())
-# print("Testing reveal_piece():     ", test_reveal_piece())
-# print("Testing valid_input():      ", test_valid_input())
-# print("Testing same_team:          ", test_same_team())
-# print("Testing move_pieces():      ", test_move_pieces())
-# print("Testing horizontal_travel():", test_horizontal_travel())
+def test_bishopValidity():
+    c = Chessboard()
+    c.move_piece(51,35)     # p to D4
+    c.move_piece(58,37)     # b to F4
+    '''{ 0:'R',  1:'N',  2:'B',  3:'Q',  4:'K',  5:'B',  6:'N',  7:'R', 
+         8:'P',  9:'P', 10:'P', 11:'P', 12:'P', 13:'P', 14:'P', 15:'P',
+        16:' ', 17:' ', 18:' ', 19:' ', 20:' ', 21:' ', 22:' ', 23:' ',
+        24:' ', 25:' ', 26:' ', 27:' ', 28:' ', 29:' ', 30:' ', 31:' ',
+        32:' ', 33:' ', 34:' ', 35:'p', 36:' ', 37:'b', 38:' ', 39:' ',
+        40:' ', 41:' ', 42:' ', 43:' ', 44:' ', 45:' ', 46:' ', 47:' ',
+        48:'p', 49:'p', 50:'p', 51:' ', 52:'p', 53:'p', 54:'p', 55:'p',
+        56:'r', 57:'n', 58:' ', 59:'q', 60:'k', 61:'b', 62:'n', 63:'r'}'''
+    try:
+        assert c.bishopValidity(37,45) == False     # move vertically
+        assert c.bishopValidity(37,36) == False     # move horizontally
+        assert c.bishopValidity(37,23) == True      # move diagonally
+        assert c.bishopValidity(37,1) == False      # jump over piece
+        assert c.bishopValidity(37,10) == True      # kill P
+        return True
+    except:
+        return False
+
+def test_queenValidity():
+    c = Chessboard()
+    c.move_piece(52,36)     #p to E4
+    c.move_piece(11,27)     #P to D5
+    c.move_piece(59,49)     #q to F3
+    c.move_piece(3,19)      #Q to D6
+    '''{ 0:'R',  1:'N',  2:'B',  3:'Q',  4:'K',  5:'B',  6:'N',  7:'R', 
+         8:'P',  9:'P', 10:'P', 11:' ', 12:'P', 13:'P', 14:'P', 15:'P',
+        16:' ', 17:' ', 18:' ', 19:' ', 20:' ', 21:' ', 22:' ', 23:' ',
+        24:' ', 25:' ', 26:' ', 27:'P', 28:' ', 29:' ', 30:' ', 31:' ',
+        32:' ', 33:' ', 34:' ', 35:' ', 36:'p', 37:' ', 38:' ', 39:' ',
+        40:' ', 41:' ', 42:' ', 43:' ', 44:' ', 45:'q', 46:' ', 47:' ',
+        48:'p', 49:'p', 50:'p', 51:'p', 52:' ', 53:'p', 54:'p', 55:'p',
+        56:'r', 57:'n', 58:'b', 59:' ', 60:'k', 61:'b', 62:'n', 63:'r'}'''
+    try:
+        assert c.queenValidity(3,19) == True    # move Q two forward
+        assert c.queenValidity(3,5) == False    # try jumping over piece
+        assert c.queenValidity(45,39) == False  # try going off the side
+        assert c.queenValidity(45,31) == True   # move diagonally
+        assert c.queenValidity(45,28) == False  # move like a (k)night
+        return True
+    except:
+        return False
+
+def test_kingValidity():
+    c = Chessboard()
+    c.move_piece(52,36)     #p to E4
+    c.move_piece(11,27)     #P to D5
+    c.move_piece(59,49)     #q to F3
+    c.move_piece(3,19)      #Q to D6
+    c.move_piece(4,31)      #K to H5 (lil cheat)
+    
+    '''{ 0:'R',  1:'N',  2:'B',  3:'Q',  4:' ',  5:'B',  6:'N',  7:'R', 
+         8:'P',  9:'P', 10:'P', 11:' ', 12:'P', 13:'P', 14:'P', 15:'P',
+        16:' ', 17:' ', 18:' ', 19:' ', 20:' ', 21:' ', 22:' ', 23:' ',
+        24:' ', 25:' ', 26:' ', 27:'P', 28:' ', 29:' ', 30:' ', 31:'K',
+        32:' ', 33:' ', 34:' ', 35:' ', 36:'p', 37:' ', 38:' ', 39:' ',
+        40:' ', 41:' ', 42:' ', 43:' ', 44:' ', 45:'q', 46:' ', 47:' ',
+        48:'p', 49:'p', 50:'p', 51:'p', 52:' ', 53:'p', 54:'p', 55:'p',
+        56:'r', 57:'n', 58:'b', 59:' ', 60:'k', 61:'b', 62:'n', 63:'r'}'''
+    try:
+        assert c.kingValidity(60,52) == True   # move k one forward
+        assert c.kingValidity(31,32) == False  # try going off the side
+        assert c.kingValidity(60,59) == True   # move to the side
+        assert c.kingValidity(31,22) == True   # move diagonally
+        assert c.kingValidity(31,38) == True  # move diagonally
+        assert c.kingValidity(31,37) == False  # move weirdly
+        return True
+    except:
+        return False
+
+print("Testing create_board():     ", test_create_board())
+print("Testing current_board():    ",test_current_board())
+print("Testing reveal_piece():     ", test_reveal_piece())
+print("Testing valid_input():      ", test_valid_input())
+print("Testing same_team:          ", test_same_team())
+print("Testing move_pieces():      ", test_move_pieces())
+print("Testing horizontal_travel():", test_horizontal_travel())
 print("Testing vertical_travel():  ", test_vertical_travel())
-#print("Testing diagonal_travel():  ", test_diagonal_travel())
+print("Testing diagonal_travel():  ", test_diagonal_travel())
+print("Testing rookValidity():    ", test_rookValidity())
+print("Testing bishopValidity():", test_bishopValidity())
+print("Testing queenValidity():", test_queenValidity())
+print("Testing kingValidity():", test_kingValidity())
